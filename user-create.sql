242b1d8002e20ca2d819004434ae610c7000eaf6
Jason Farrell <imjason@gmail.com>
2022-01-16 23:01:23 -0500

    tweaks to the database creation code

--- user-create.sql
+++ user-create.sql
@@ -7,8 +7,6 @@ ALTER ROLE db_datawriter ADD MEMBER [id-pocapplication-pojm2j];
 GRANT EXECUTE TO [id-pocapplication-pojm2j]
 GO
 
-Server=tcp:sqlserver-pocapplication-pojm2j.database.windows.net;User Id=azureuser; Password=TempPassword011!!X980; Database=db-pocapplication-pojm2j;
-
 insert into [dbo].[People] values(newid(), 'Jason', 'Farrell')
 GO
 
